82c1a9378b8ad9ef7c000a6ed7d93708e110ec12
Alfredo Garcia <oxarbitrage@gmail.com>
2019-10-24 14:03:57 -0300

    add balances to get blinded accounts

--- plugins/getters/getmyblindaccounts.cpp
+++ plugins/getters/getmyblindaccounts.cpp
@@ -10,7 +10,7 @@ GetMyBlindAccounts::GetMyBlindAccounts(GWallet* gwallet) : wxScrolledWindow() {
 
    SetScrollRate(1, 1);
 
-   response_grid->CreateGrid(0, 2);
+   response_grid->CreateGrid(0, 3);
 
    SetScrollRate(1, 1);
    response_grid->ShowScrollbars(wxSHOW_SB_NEVER, wxSHOW_SB_NEVER);
@@ -19,6 +19,7 @@ GetMyBlindAccounts::GetMyBlindAccounts(GWallet* gwallet) : wxScrolledWindow() {
 
    response_grid->SetColLabelValue(0, _("Label"));
    response_grid->SetColLabelValue(1, _("Public key"));
+   response_grid->SetColLabelValue(2, _("Balances"));
 
    stringstream command;
    command << "get_my_blind_accounts";
@@ -29,7 +30,7 @@ GetMyBlindAccounts::GetMyBlindAccounts(GWallet* gwallet) : wxScrolledWindow() {
 
    if (!response.IsNull()) {
       auto results = response.As<map<string, public_key_type>>();
-      auto z = 0;
+      auto counter = 0;
 
       response_grid->BeginBatch();
       response_grid->ClearSelection();
@@ -37,9 +38,28 @@ GetMyBlindAccounts::GetMyBlindAccounts(GWallet* gwallet) : wxScrolledWindow() {
       response_grid->AppendRows(results.size());
       for (auto &blind : results) {
 
-         response_grid->SetCellValue(z, 0, blind.first);
-         response_grid->SetCellValue(z, 1, string(blind.second));
-         ++z;
+         response_grid->SetCellValue(counter, 0, blind.first);
+         response_grid->SetCellValue(counter, 1, string(blind.second));
+
+         stringstream balances_string;
+
+         const auto balances = p_GWallet->bitshares.wallet_api_ptr->get_blind_balances(blind.first);
+         if(balances.size() > 0)
+         {
+            for(auto &balance : balances)
+            {
+               // Todo: create a function
+               const std::string asset_id = fc::to_string(balance.asset_id.space_id) +
+                                      "." + fc::to_string(balance.asset_id.type_id) +
+                                      "." + fc::to_string(balance.asset_id.instance.value);
+
+               const auto asset = p_GWallet->bitshares.wallet_api_ptr->get_asset(asset_id);
+               balances_string << asset.symbol << ":" << p_GWallet->DoPrettyBalance(asset.precision, balance.amount.value) << " ";
+            }
+         }
+         response_grid->SetCellValue(counter, 2, balances_string.str());
+
+         ++counter;
       }
       response_grid->AutoSize();
       response_grid->ForceRefresh();
